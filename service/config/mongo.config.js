25ed747c7108e66837edc7a040dc819a65798e84
Mauricio <136762122+Mauricio-Mtz@users.noreply.github.com>
2025-03-05 22:51:42 -0600

    fix7

--- service/config/mongo.config.js
+++ service/config/mongo.config.js
@@ -1,19 +1,12 @@
 const mongoose = require('mongoose');
 
-// Conexión a la base de datos
-const mongoDb = mongoose.createConnection('mongodb+srv://2022371199:BTEU4sBy1MLTAXjm@task-manager.1z4y8.mongodb.net/?retryWrites=true&w=majority&appName=task-manager', {
+mongoose.connect('mongodb+srv://2022371199:BTEU4sBy1MLTAXjm@task-manager.1z4y8.mongodb.net/?retryWrites=true&w=majority&appName=task-manager', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+    ssl: true,
+    tls: true
+})
+.then(() => console.info('Conexión MongoDB lista'))
+.catch(error => console.error('Error de conexión MongoDB:', error));
 
-// En caso de que la conexion sea erronea
-mongoDb.on('error', (error) => {
-    console.error('Error de conexión MongoDB:', error);
-});
-
-// En caso de que la conexion sea exitosa
-mongoDb.once('open', () => {
-    console.info('Conexión MongoDB lista');
-});
-
-module.exports = mongoDb;
+module.exports = mongoose.connection;
